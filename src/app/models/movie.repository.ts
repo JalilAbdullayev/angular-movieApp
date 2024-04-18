aa6e2e6c3dc670998f7c9fd511410e6fb2a9d745
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-18 06:43:38 +0400

    Popular Movies

--- src/app/models/movie.repository.ts
+++ src/app/models/movie.repository.ts
@@ -8,27 +8,35 @@ export class MovieRepository {
       id: 1,
       title: 'Movie 1',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, quidem.',
-      imageUrl: 'jurassic-park.jpg'
+      imageUrl: 'jurassic-park.jpg',
+      isPopular: true
     }, {
       id: 2,
       title: 'Movie 2',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, quidem.',
-      imageUrl: 'godzilla.jpg'
+      imageUrl: 'godzilla.jpg',
+      isPopular: false
     }, {
       id: 3,
       title: 'Movie 3',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, quidem.',
-      imageUrl: 'the-hunger-games.jpg'
+      imageUrl: 'the-hunger-games.jpg',
+      isPopular: false
     }, {
       id: 4,
       title: 'Movie 4',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem, quidem.',
-      imageUrl: 'rogue-one.jpg'
+      imageUrl: 'rogue-one.jpg',
+      isPopular: true
     }];
   }
 
   getMovies(): Movie[] {
     return this.movies;
+  }
+
+  getPopularMovies(): Movie[] {
+    return this.movies.filter(m => m.isPopular);
   }
 
   getMovieById(id: number): Movie {
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -10,10 +10,12 @@ import {MovieRepository} from "../models/movie.repository";
 export class MoviesComponent {
   title = 'Movies List';
   movies: Movie[];
+  popularMovies: Movie[];
   movieRepository: MovieRepository;
 
   constructor() {
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getMovies();
+    this.popularMovies = this.movieRepository.getPopularMovies();
   }
 }
