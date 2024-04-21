a02c4aa3430e0b05878eae9e7f74082a60a62df7
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-21 16:57:35 +0400

    Template-driven Form

--- src/app/movies/movie-create/movie-create.component.ts
+++ src/app/movies/movie-create/movie-create.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, model, OnInit} from '@angular/core';
 import {CategoryService} from "../../services/category.service";
 import {Category} from "../../models/category";
 import {MovieService} from "../../services/movie.service";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
 import {AlertifyService} from "../../services/alertify.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-movie-create',
@@ -14,7 +15,7 @@ import {AlertifyService} from "../../services/alertify.service";
 })
 export class MovieCreateComponent implements OnInit {
   categories: Category[];
-  error: string;
+  model: any = {};
 
   constructor(private categoryService: CategoryService,
               private movieService: MovieService,
@@ -29,34 +30,13 @@ export class MovieCreateComponent implements OnInit {
     })
   }
 
-  createMovie(title: any, description: any, imageUrl: any, categoryId: any) {
-    if (!title.value || !description.value || !imageUrl.value || categoryId.value === '-1') {
-      this.alertify.error('All fields are required!');
-      return;
-    }
-
-    if (title.value.length < 5) {
-      this.alertify.error('Title must be at least 5 characters long!');
-      return;
-    }
-
-    if (description.value.length < 10 || description.value.length > 50) {
-      this.alertify.error('Description must be between 10 and 50 characters long!');
-      return;
-    }
-
-    const extensions = ['jpg', 'jpeg', 'png'];
-    const extension = imageUrl.value.split('.').pop();
-    if (extensions.indexOf(extension) === -1) {
-      this.alertify.error('Supported image extensions: jpg, jpeg, png');
-      return;
-    }
-
+  createMovie(form: NgForm) {
     this.http.get<any[]>('http://localhost:3000/movies').subscribe(data => {
       const lastMovie = data[data.length - 1];
       const id = lastMovie ? Number(lastMovie.id) + 1 : 1;
-
-      const movie = {
+      console.log(this.model);
+      console.log(form)
+      /*const movie = {
         id: id.toString(),
         title: title.value,
         description: description.value,
@@ -68,7 +48,11 @@ export class MovieCreateComponent implements OnInit {
 
       this.movieService.createMovie(movie).subscribe(data => {
         this.router.navigate(['/movies', data.id]);
-      })
+      })*/
     })
+  }
+
+  log(value: any) {
+    console.log(value)
   }
 }
