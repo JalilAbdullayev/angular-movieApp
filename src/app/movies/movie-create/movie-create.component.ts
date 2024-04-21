040b1f076c6bc87b2c6e65f79d67f7193e0769ad
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-21 16:38:55 +0400

    Form Validation

--- src/app/movies/movie-create/movie-create.component.ts
+++ src/app/movies/movie-create/movie-create.component.ts
@@ -4,6 +4,7 @@ import {Category} from "../../models/category";
 import {MovieService} from "../../services/movie.service";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {AlertifyService} from "../../services/alertify.service";
 
 @Component({
   selector: 'app-movie-create',
@@ -13,8 +14,13 @@ import {HttpClient} from "@angular/common/http";
 })
 export class MovieCreateComponent implements OnInit {
   categories: Category[];
+  error: string;
 
-  constructor(private categoryService: CategoryService, private movieService: MovieService, private router: Router, private http: HttpClient) {
+  constructor(private categoryService: CategoryService,
+              private movieService: MovieService,
+              private router: Router,
+              private http: HttpClient,
+              private alertify: AlertifyService) {
   }
 
   ngOnInit(): void {
@@ -24,9 +30,30 @@ export class MovieCreateComponent implements OnInit {
   }
 
   createMovie(title: any, description: any, imageUrl: any, categoryId: any) {
+    if (!title.value || !description.value || !imageUrl.value || categoryId.value === '-1') {
+      this.alertify.error('All fields are required!');
+      return;
+    }
+
+    if (title.value.length < 5) {
+      this.alertify.error('Title must be at least 5 characters long!');
+      return;
+    }
+
+    if (description.value.length < 10 || description.value.length > 50) {
+      this.alertify.error('Description must be between 10 and 50 characters long!');
+      return;
+    }
+
+    const extensions = ['jpg', 'jpeg', 'png'];
+    const extension = imageUrl.value.split('.').pop();
+    if (extensions.indexOf(extension) === -1) {
+      this.alertify.error('Supported image extensions: jpg, jpeg, png');
+      return;
+    }
+
     this.http.get<any[]>('http://localhost:3000/movies').subscribe(data => {
       const lastMovie = data[data.length - 1];
-
       const id = lastMovie ? Number(lastMovie.id) + 1 : 1;
 
       const movie = {
