5f39077d90dfc161a2ed8b6607afdc449b993a14
JalilAbdullayev <abdullacelil00@gmail.com>
2024-05-28 06:40:36 +0400

    Auto Login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -22,14 +22,7 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     }).pipe(tap(response => {
-      const expirationDate = new Date(new Date().getTime() + (+response.expiresIn) * 1000);
-      const user = new User(
-        response.email,
-        response.localId,
-        response.idToken,
-        expirationDate
-      )
-      this.user.next(user);
+      this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn);
     }));
   }
 
@@ -39,19 +32,41 @@ export class AuthService {
       password: password,
       returnSecureToken: true
     }).pipe(tap(response => {
-      const expirationDate = new Date(new Date().getTime() + (+response.expiresIn) * 1000);
-      const user = new User(
-        response.email,
-        response.localId,
-        response.idToken,
-        expirationDate
-      )
-      this.user.next(user);
+      this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn);
     }));
   }
 
   logOut() {
     this.user.next(null);
+    localStorage.removeItem('user');
     this.router.navigate(['/auth']);
+  }
+
+  autoLogin() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (!user) {
+      return;
+    }
+    const loadedUser = new User(
+      user.email,
+      user.id,
+      user._token,
+      new Date(user._tokenExpirationDate)
+    )
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
+  handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + (expiresIn * 1000));
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    )
+    this.user.next(user);
+    localStorage.setItem('user', JSON.stringify(user))
   }
 }
